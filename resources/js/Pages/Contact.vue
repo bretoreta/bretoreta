b7258e2831ac68ce9ea56aaef097db30903aa1dd
bretoreta <bremmurd08@gmail.com>
2022-12-26 13:05:04 +0300

    Feature:Completed Contact Page

--- resources/js/Pages/Contact.vue
+++ resources/js/Pages/Contact.vue
@@ -1,8 +1,30 @@
 <script setup>
-    import { Head, Link } from '@inertiajs/inertia-vue3';
+    import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
     import Header from '@/Components/Header.vue';
     import Footer from '@/Components/Footer.vue';
     import PrimaryButton from '@/Components/PrimaryButton.vue'
+    import Checkbox from '@/Components/Checkbox.vue';
+    import InputError from '@/Components/InputError.vue';
+    import InputLabel from '@/Components/InputLabel.vue';
+    import TextInput from '@/Components/TextInput.vue';
+    import TextArea from '@/Components/TextArea.vue';
+
+    const form = useForm({
+        email: '',
+        phone: '',
+        name: '',
+        message: '',
+        subscribe: true,
+    });
+
+    const submit = () => {
+        form.transform(data => ({
+            ...data,
+            subscribe: form.subscribe ? 'on' : '',
+        })).post(route('login'), {
+            onFinish: () => form.reset('password'),
+        });
+    };
 </script>
 
 <template>
@@ -18,6 +40,91 @@
                         <div class="bg-gray-900/60 rounded-full w-40 h-40 absolute -top-12 left-72 -z-10" />
                         <div class="text-sm font-semibold text-white">Message &middot; Inquiry &middot; Suggestion</div>
                         <h4 class="text-6xl lg:text-display font-bold text-white leading-tight">Contact Me</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="contact bg-gray-100 p-40 w-full">
+            <div class="grid grid-cols-4 items-center">
+                <div class="col-span-2">
+                    <div>
+                        <h4 class="text-7xl text-gray-700 font-bold">Leave A Message</h4>
+                        <p class="text-gray-500 mt-4">Inquire about my services, write a message to me or maybe just have a chat?</p>
+                    </div>
+                    <div class="mt-10">
+                        <form @submit.prevent="submit">
+                            <div>
+                                <InputLabel for="email" value="Email" />
+                                <TextInput
+                                    id="email"
+                                    v-model="form.email"
+                                    type="email"
+                                    class="mt-1 block w-full"
+                                    required
+                                    autofocus
+                                />
+                                <InputError class="mt-2" :message="form.errors.email" />
+                            </div>
+
+                            <div class="mt-4">
+                                <InputLabel for="name" value="Name" />
+                                <TextInput
+                                    id="name"
+                                    v-model="form.name"
+                                    type="text"
+                                    class="mt-1 block w-full"
+                                    required
+                                />
+                                <InputError class="mt-2" :message="form.errors.name" />
+                            </div>
+
+                            <div class="mt-4">
+                                <InputLabel for="phone" value="Phone Number" />
+                                <TextInput
+                                    id="phone"
+                                    v-model="form.phone"
+                                    type="tel"
+                                    class="mt-1 block w-full"
+                                    required
+                                />
+                                <InputError class="mt-2" :message="form.errors.phone" />
+                            </div>
+
+                            <div class="mt-4">
+                                <InputLabel for="message" value="Message" />
+                                <TextArea
+                                    id="message"
+                                    :rows="4"
+                                    v-model="form.message"
+                                    class="mt-1 block w-full"
+                                    required
+                                />
+                                <InputError class="mt-2" :message="form.errors.message" />
+                            </div>
+
+                            <div class="block mt-4">
+                                <label class="flex items-center">
+                                    <Checkbox v-model:checked="form.subscribe" name="subscribe" />
+                                    <span class="ml-2 text-sm text-gray-600">Subscribe To Maling List</span>
+                                </label>
+                            </div>
+
+                            <div class="flex items-center justify-end mt-4">
+                                <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
+                                    Forgot your password?
+                                </Link>
+
+                                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                                    Send Message
+                                </PrimaryButton>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-span-2">
+                    <div>
+                        <img src="../../assets/images/contact_me_doodle.svg" alt="Contact Me Doodle" class="mx-auto w-80">
                     </div>
                 </div>
             </div>
