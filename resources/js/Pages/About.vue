d0a3f1e94a9096b6753e84afe9d45c448bf8dce8
bretoreta <bremmurd08@gmail.com>
2022-12-26 11:46:52 +0300

    Feature:Completed About Me Page

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -26,16 +26,163 @@
         <div class="bg-gray-100 p-40 w-full">
             <h4 class="text-7xl text-gray-700 font-bold">My Experience</h4>
             <div class="mt-10">
-                <h4 class="text-xl font-semibold text-gray-700">Lead Designer</h4>
-                <div class="flex items-center space-x-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                    <p class="text-gray-500 text-sm">2019 March - Current</p>
+                <h4 class="text-2xl font-semibold text-gray-700">Full Stack Developer - Remote</h4>
+                <div class="flex items-center space-x-6 mt-2">
+                    <div class="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        <p class="text-gray-500 text-sm">2021 September - Current</p>
+                    </div>
+                    <div class="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+                        </svg>
+                        <p class="text-gray-500 text-sm">ClicksTribute</p>
+                    </div>
                 </div>
                 <div class="mt-5">
-                    <p class="text-gray-700">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus, eligendi hic numquam eos dolore enim facere totam a? Neque quas veniam libero quaerat error tenetur sapiente hic cupiditate, fugit harum consequatur quidem culpa, dolores rem ad! Accusamus alias, eligendi ratione veniam repellat voluptatem omnis quod, ducimus magni aliquid nostrum pariatur similique, eaque porro quis accusantium aperiam rem! Saepe minus numquam quibusdam delectus hic cum nisi soluta excepturi eos libero? Minima perferendis eos voluptates. Est eius, labore autem doloribus esse, blanditiis aliquam animi pariatur unde, laboriosam exercitationem eos neque nulla sed dolorum libero possimus sit molestiae perspiciatis velit iste temporibus repellendus?</p>
-                    <p class="text-gray-700 mt-3">Fugiat neque doloremque culpa ducimus. Delectus animi maiores inventore unde modi temporibus labore minima placeat ea totam sequi porro, vitae quas similique laborum perferendis neque, dolore facere atque iste odit. Ipsam hic laboriosam corrupti, pariatur delectus animi voluptatem suscipit ad! Eum itaque laborum iste perferendis voluptatibus nisi corrupti tenetur, beatae blanditiis perspiciatis voluptatem recusandae molestias optio officiis possimus nesciunt rerum! Totam, dolore alias quas rem officia autem! Iure mollitia consectetur id quas illum corrupti dignissimos quibusdam, omnis fuga similique earum, deleniti incidunt quaerat culpa quis, totam illo alias nostrum atque vitae veritatis animi a. Doloribus voluptates pariatur praesentium similique minus?</p>
+                    <p class="text-gray-700">In my role, I am responsible for the full application development cycle. As a full-stack developer, I am responsible for the frontend, backend as well as devops. I am also a key contributor to the application's design patterns and user experience designs: I have contributed to the logo design, landing page design, and the general app look and feel. Coming up with a working color scheme from the logo is also my job.</p>
+                    <p class="text-gray-700 mt-3">I also contribute a key role in debugging and fixing issues with the application in development. I identify, assess, verify and come up with mitigating methods and solutions for the bugs discovered within the system. I am also responsible for handling the security of users who will be using the system, ensuring that their data remains confidential and that the system is strongly protected from attackers and or hackers.</p>
+                </div>
+            </div>
+            <div class="mt-10">
+                <h4 class="text-2xl font-semibold text-gray-700">Backend Developer - Remote</h4>
+                <div class="flex items-center space-x-6 mt-2">
+                    <div class="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        <p class="text-gray-500 text-sm">2021 February - 2021 March</p>
+                    </div>
+                    <div class="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+                        </svg>
+                        <p class="text-gray-500 text-sm">Worksson Inc.</p>
+                    </div>
+                </div>
+                <div class="mt-5">
+                    <p class="text-gray-700">In my role, I was involved in the full-application development cycle and participated in development team stand-ups and scrums to meet aggressive deadlines. I, together with a team of other developers, were responsible for building out a strong, extensive, secure, robust and scalable backend for the platform, which we delivered without a doubt.</p>
+                    <p class="text-gray-700 mt-3">I also participated in coming up with amicable solutions to the existing user interface designs in order to give the website a new look. The existing user interface was not the best so we had to come up with another design that would give users the best experience using the platform.</p>
+                </div>
+            </div>
+            <div class="mt-10">
+                <h4 class="text-2xl font-semibold text-gray-700">Web Developer & Designer - On Site</h4>
+                <div class="flex items-center space-x-6 mt-2">
+                    <div class="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        <p class="text-gray-500 text-sm">2019 March - 2020 September</p>
+                    </div>
+                    <div class="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+                        </svg>
+                        <p class="text-gray-500 text-sm">Inkpot Group LLC</p>
+                    </div>
+                </div>
+                <div class="mt-5">
+                    <p class="text-gray-700">In my role I was also assigned design and development jobs tasked to the company and delivered all jobs on time. I was assigned the role of designing and coming up with the company logo and other official documents e.g. Invoice document</p>
+                    <p class="text-gray-700 mt-3">I developed and made logos for Belya Fiber Exporters and Prime Charters International. The official logos for both projects are used in their live projects, also developed by me, and the live projects can be accessed using these links: <a class="text-sky-500 hover:underline" href="https://belyafiberexporters.co.ke" target="_blank" rel="noopener noreferrer">Belya Fiber Exporters</a> and <a class="text-sky-500 hover:underline" href="https://primechartersinternational.co.ke" target="_blank" rel="noopener noreferrer">Prime Charters International</a></p>
+                </div>
+            </div>
+            <div class="mt-10">
+                <h4 class="text-2xl font-semibold text-gray-700">Lead Designer</h4>
+                <div class="flex items-center space-x-6 mt-2">
+                    <div class="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        <p class="text-gray-500 text-sm">2019 March - 2020 September</p>
+                    </div>
+                    <div class="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+                        </svg>
+                        <p class="text-gray-500 text-sm">Inkpot Group LLC</p>
+                    </div>
+                </div>
+                <div class="mt-5">
+                    <p class="text-gray-700">Being the lead designer, I was in charge of coming up with several layouts for the magazine templates while keeping simplicity of the magazine uncompromised. I participated in making sure every release of a fresh edition of the magazine is clean and doesn’t in any way imitate the layout of any existing magazine.</p>
+                    <p class="text-gray-700 mt-3">As a lead designer, I was assigned a team of other designers who we would work with hand-in-hand to come up with designs for the magazine. I was responsible for handling the job of managing my team's work while upholding the company's design guidelines and standards set.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="bg-lime-600 p-40 w-full">
+            <div class="grid grid-cols-4 items-center">
+                <div class="col-span-2">
+                    <h4 class="text-7xl text-white font-bold">My Education</h4>
+                    <div class="mt-10">
+                        <h4 class="text-2xl font-semibold text-lime-800">Bsc. Computer Science</h4>
+                        <div class="flex items-center space-x-6 mt-2">
+                            <div class="flex items-center space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6 text-lime-800">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                                <p class="text-lime-300 text-sm">Joined 2018 September</p>
+                            </div>
+                            <div class="flex items-center space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-lime-800">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+                                </svg>
+                                <p class="text-lime-300 text-sm">Maseno University - Kenya</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-10">
+                        <h4 class="text-2xl font-semibold text-lime-900">Kenya Certificate of Secondary Education</h4>
+                        <div class="flex items-center space-x-6 mt-2">
+                            <div class="flex items-center space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6 text-lime-800">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                                <p class="text-lime-300 text-sm">Joined 2014 January</p>
+                            </div>
+                            <div class="flex items-center space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-lime-800">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+                                </svg>
+                                <p class="text-lime-300 text-sm">Agoro Sare High School - Kenya</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-10">
+                        <h4 class="text-2xl font-semibold text-lime-900">Kenya Certificate of Primary Education</h4>
+                        <div class="flex items-center space-x-6 mt-2">
+                            <div class="flex items-center space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6 text-lime-800">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                                <p class="text-lime-300 text-sm">Joined 2006 January</p>
+                            </div>
+                            <div class="flex items-center space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-lime-800">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+                                </svg>
+                                <p class="text-lime-300 text-sm">Duha Primary School - Kenya</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-span-2">
+                    <img src="../../assets/images/my_education_doodle.svg" alt="My Education Doodle" class="w-60 mx-auto">
+                </div>
+            </div>
+        </div>
+
+        <div class="bg-gray-100 p-40 w-full">
+            <div class="grid grid-cols-4 items-center">
+                <div class="col-span-2">
+                    <img src="../../assets/images/my_hobbies_doodle.svg" alt="My Hobbies Doodle" class="w-60 mx-auto">
+                </div>
+                <div class="col-span-2">
+                    <h4 class="text-7xl text-gray-700 font-bold">My Hobbies</h4>
+                    <div class="mt-10">
+                        <h4 class="text-2xl font-semibold text-gray-700">Watching YouTube Tutorials, Drawing, Playing Mobile Games, Walking With My Dog.</h4>
+                    </div>
                 </div>
             </div>
         </div>
