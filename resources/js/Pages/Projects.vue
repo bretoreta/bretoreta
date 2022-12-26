732c3d906988013508d387f3869be013dd16cee0
bretoreta <bremmurd08@gmail.com>
2022-12-26 12:19:16 +0300

    Feature:Completed Projects Page

--- resources/js/Pages/Projects.vue
+++ resources/js/Pages/Projects.vue
@@ -23,6 +23,114 @@
             </div>
         </div>
 
+        <div class="bg-gray-100 p-40 w-full">
+            <h4 class="text-7xl text-gray-700 font-bold">My Projects</h4>
+            <div class="mt-10">
+                <h4 class="text-2xl font-semibold text-gray-700">Portfolio - Personal Website</h4>
+                <div class="flex items-center space-x-6 mt-2">
+                    <div class="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        <p class="text-gray-500 text-sm">2021 September - Current</p>
+                    </div>
+                    <div class="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+                        </svg>
+                        <p class="text-gray-500 text-sm">Self Employed</p>
+                    </div>
+                    <div class="flex space-x-2 items-center text-blue-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+                        </svg>
+                        <p class="text-sm font-semibold">https://bretoreta.me</p>
+                    </div>
+                </div>
+                <div class="mt-5">
+                    <p class="text-gray-700">This is a project I have thought of over the years and finally got to make it possible. While developing this project, I learned new technologies and CSS properties e.g. background-clipping that I have used widely in this project. This was a personal inspiration to make a clean minimal and expressive website for my self and eventually turn in into a blog. I am yet to realise this dream but plans are in place.</p>
+                </div>
+            </div>
+            <div class="mt-10">
+                <h4 class="text-2xl font-semibold text-gray-700">Electra - Online Voting Platform</h4>
+                <div class="flex items-center space-x-6 mt-2">
+                    <div class="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        <p class="text-gray-500 text-sm">2018 Sept - Actively Developing</p>
+                    </div>
+                    <div class="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+                        </svg>
+                        <p class="text-gray-500 text-sm">Self Employed</p>
+                    </div>
+                    <div class="flex space-x-2 items-center text-blue-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+                        </svg>
+                        <p class="text-sm font-semibold">https://electravoting.online</p>
+                    </div>
+                </div>
+                <div class="mt-5">
+                    <p class="text-gray-700">Soon as I joined campus I had a dream, of builing my own system then use it as a SAAS. I had several ideas in mind but after a long mind battle of what I deserve best and what the market needs, I settled on building an online voting platform. As simple as it seems this project has taken most of my time as it required a lot of research. I released the first verion on <a class="text-blue-600 font-semibold hover:underline" href="http://github.com/bretoreta/electra" target="_blank" rel="noopener noreferrer">GitHub</a> and have been working on an update to that for the past 1 Year. I know 1 year is such a long time, but you must consider I was a student then, and didn't have so much time available. The project is under active development and I hope to put up a team and make Electra an inernationally recognized SAAS.</p>
+                </div>
+            </div>
+            <div class="mt-10">
+                <h4 class="text-2xl font-semibold text-gray-700">Netflix Clone - Cloned Platform</h4>
+                <div class="flex items-center space-x-6 mt-2">
+                    <div class="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        <p class="text-gray-500 text-sm">2019 January - 2019 February</p>
+                    </div>
+                    <div class="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+                        </svg>
+                        <p class="text-gray-500 text-sm">Self Employed</p>
+                    </div>
+                    <div class="flex space-x-2 items-center text-blue-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+                        </svg>
+                        <p class="text-sm font-semibold">https://github.com/bretoreta/netflix-clone</p>
+                    </div>
+                </div>
+                <div class="mt-5">
+                    <p class="text-gray-700">The major purpose of this project was to learn React, a popular frontend technology that is widely used among the tech community. I however fell apart with React after I met Vue Js which, according to me, was easier to learn and I quickly grasped Vue's concepts. I however challenged myself to build a react project and made it happen. The project is not hosted however, the project files can be found on the GitHub link mentioned above. The project is very basic and only replicates the basic Netflix use bases i.e. showing movies on homepage and playing trailers if any, only. Plans are in place to make it live.</p>
+                </div>
+            </div>
+            <div class="mt-10">
+                <h4 class="text-2xl font-semibold text-gray-700">Tuska - Task Management Platform</h4>
+                <div class="flex items-center space-x-6 mt-2">
+                    <div class="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        <p class="text-gray-500 text-sm">Concept</p>
+                    </div>
+                    <div class="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+                        </svg>
+                        <p class="text-gray-500 text-sm">Self Employed</p>
+                    </div>
+                    <div class="flex space-x-2 items-center text-blue-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+                        </svg>
+                        <p class="text-sm font-semibold">Concept</p>
+                    </div>
+                </div>
+                <div class="mt-5">
+                    <p class="text-gray-700">The idea is to have a simple task management system that low end product consumers can use. I had already built a simple task management system and is avalable on my <a class="text-blue-600 font-semibold hover:underline" href="http://github.com/bretoreta/pipe" target="_blank" rel="noopener noreferrer">GitHub</a>, but the project lacked so many aspects of a task management system. It had so many shortcomings, one key shortcoming being lack of tasks tracking: the project had no way of tracking time used for each task and that was a major concern. I decided to scrap the whole project and I'm now having this new concept that I'd like to visualize. The app will probably take me longer to complete but it's a work in progress.</p>
+                </div>
+            </div>
+        </div>
+
         <Footer />
     </div>
 </template>
