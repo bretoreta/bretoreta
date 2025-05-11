12fe8167ff7fff35b46ad2258cb0e87a12e91975
Bret <bremmurd08@gmail.com>
2025-05-11 03:25:21 +0300

    Feat:Add Hero Interactive grid pattern

---
+++ components/robot-model.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { Application } from '@splinetool/runtime';
+
+const canvasRef = ref(null);
+
+onMounted(() => {
+  const app = new Application(canvasRef.value);
+  app.load('https://prod.spline.design/28Ik02AhGiPFtrEW/scene.splinecode');
+});
+</script>
+
+<template>
+  <div class="spline-wrapper w-full h-[80vh] max-h-[800px] mx-auto">
+    <canvas 
+      ref="canvasRef" 
+      class="w-full h-full rounded-xl shadow-lg"
+    />
+  </div>
+</template>
